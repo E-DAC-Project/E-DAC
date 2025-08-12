1846a36d77348cb218a2fb657f32c766fc7faa2b
89257-Yashraj <yashrajshelar7514@gmail.com>
2025-08-12 14:25:35 +0530

    changes

--- edacbackend/src/main/java/com/sunbeam/controller/TopicController.java
+++ edacbackend/src/main/java/com/sunbeam/controller/TopicController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping; // Added for DELETE
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable; // Added for PathVariable
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping; // Added for PUT
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +22,15 @@ import com.sunbeam.entities.Topics; // Import Topics entity
 import lombok.AllArgsConstructor;
 
 @RestController
+@RequestMapping("/topics")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class TopicController {
 
 	private final TopicService topicService;
 	
-	@GetMapping("/topics")
-	public ResponseEntity<?> getTopicsByModule(@RequestParam Long id) {
+	@GetMapping("/getTopics/{id}")
+	public ResponseEntity<?> getTopicsByModule(@PathVariable Long id) {
 		
 		List<TopicDto> topicList =  topicService.getAllTopicByModule(id);
 		if(topicList.isEmpty()) {
