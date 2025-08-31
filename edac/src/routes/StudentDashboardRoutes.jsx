4d46706e34662432b4371bc71d4412d160584024
89257-Yashraj <yashrajshelar7514@gmail.com>
2025-08-31 23:03:56 +0530

    added docker configuration to deploy project on docker

--- edac/src/routes/StudentDashboardRoutes.jsx
+++ edac/src/routes/StudentDashboardRoutes.jsx
@@ -3,10 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import StudentDashboard from "../dashboards/StudentDashboard";
 import MyProfile from "../student/MyProfile";
-import MyCourse from "../student/MyCourse";
-import Certificate from "../student/Certificate";
-import OpenProjects from "../student/OpenProjects";
-import Settings from "../student/Settings";
+import AdminCourses from "../admin/AdminCourses";
 
 function StudentDashboardRoutes() {
   return (
@@ -14,10 +11,7 @@ function StudentDashboardRoutes() {
       <Route path="/" element={<StudentDashboard />}>
         <Route index element={<Navigate to="profile" replace />} />
         <Route path="profile" element={<MyProfile />} />
-        <Route path="course" element={<MyCourse />} />
-        <Route path="certificate" element={<Certificate />} />
-        <Route path="projects" element={<OpenProjects />} />
-        <Route path="settings" element={<Settings />} />
+        <Route path="courses" element={<AdminCourses />} />
       </Route>
     </Routes>
   );
